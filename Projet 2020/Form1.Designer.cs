418f32ac7300b3313263385345ede1b2148fbe10
anthod57 <87667439+anthod57@users.noreply.github.com>
2021-07-19 20:07:57 +0200

    Projet 2020
    Programme permettant de récupérer des données de différents capteurs (température, évaporation de l'eau, mesure du CO2) envoyées en UART depuis un microcontrôleur PIC.
    
    Les données sont ensuite affichées sur une interface.

---
+++ Projet 2020/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Projet_2020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.circularProgressBar3 = new CircularProgressBar();
+            this.circularProgressBar2 = new CircularProgressBar();
+            this.circularProgressBar1 = new CircularProgressBar();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ouvrir Port";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(9, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(303, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 50);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Fermer Port";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Temperature";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(222, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "CO2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(345, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 21);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Evaporation";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 109);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuration";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(145, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 33);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Enregister";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(142, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Unité Temperature";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "°C",
+            "°F"});
+            this.comboBox2.Location = new System.Drawing.Point(145, 39);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(130, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 79);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            115200,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(130, 20);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Vitesse (Bauds)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Port";
+            // 
+            // circularProgressBar3
+            // 
+            this.circularProgressBar3.BackColor = System.Drawing.SystemColors.Control;
+            this.circularProgressBar3.BarColor1 = System.Drawing.Color.Red;
+            this.circularProgressBar3.BarColor2 = System.Drawing.Color.Red;
+            this.circularProgressBar3.BarWidth = 8F;
+            this.circularProgressBar3.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularProgressBar3.ForeColor = System.Drawing.Color.DimGray;
+            this.circularProgressBar3.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
+            this.circularProgressBar3.LineColor = System.Drawing.Color.DimGray;
+            this.circularProgressBar3.LineWidth = 1;
+            this.circularProgressBar3.Location = new System.Drawing.Point(326, 127);
+            this.circularProgressBar3.Maximum = ((long)(255));
+            this.circularProgressBar3.MinimumSize = new System.Drawing.Size(100, 100);
+            this.circularProgressBar3.Name = "circularProgressBar3";
+            this.circularProgressBar3.ProgressShape = CircularProgressBar._ProgressShape.Flat;
+            this.circularProgressBar3.Size = new System.Drawing.Size(145, 145);
+            this.circularProgressBar3.TabIndex = 11;
+            this.circularProgressBar3.Text = "0";
+            this.circularProgressBar3.TextMode = CircularProgressBar._TextMode.Value;
+            this.circularProgressBar3.Value = ((long)(0));
+            // 
+            // circularProgressBar2
+            // 
+            this.circularProgressBar2.BackColor = System.Drawing.SystemColors.Control;
+            this.circularProgressBar2.BarColor1 = System.Drawing.Color.Red;
+            this.circularProgressBar2.BarColor2 = System.Drawing.Color.Red;
+            this.circularProgressBar2.BarWidth = 8F;
+            this.circularProgressBar2.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularProgressBar2.ForeColor = System.Drawing.Color.DimGray;
+            this.circularProgressBar2.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
+            this.circularProgressBar2.LineColor = System.Drawing.Color.DimGray;
+            this.circularProgressBar2.LineWidth = 1;
+            this.circularProgressBar2.Location = new System.Drawing.Point(173, 127);
+            this.circularProgressBar2.Maximum = ((long)(255));
+            this.circularProgressBar2.MinimumSize = new System.Drawing.Size(100, 100);
+            this.circularProgressBar2.Name = "circularProgressBar2";
+            this.circularProgressBar2.ProgressShape = CircularProgressBar._ProgressShape.Flat;
+            this.circularProgressBar2.Size = new System.Drawing.Size(145, 145);
+            this.circularProgressBar2.TabIndex = 9;
+            this.circularProgressBar2.Text = "0";
+            this.circularProgressBar2.TextMode = CircularProgressBar._TextMode.Value;
+            this.circularProgressBar2.Value = ((long)(0));
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.circularProgressBar1.BarColor1 = System.Drawing.Color.Red;
+            this.circularProgressBar1.BarColor2 = System.Drawing.Color.Red;
+            this.circularProgressBar1.BarWidth = 6F;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.DimGray;
+            this.circularProgressBar1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
+            this.circularProgressBar1.LineColor = System.Drawing.Color.DimGray;
+            this.circularProgressBar1.LineWidth = 1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(12, 127);
+            this.circularProgressBar1.Maximum = ((long)(255));
+            this.circularProgressBar1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.ProgressShape = CircularProgressBar._ProgressShape.Flat;
+            this.circularProgressBar1.Size = new System.Drawing.Size(145, 145);
+            this.circularProgressBar1.TabIndex = 7;
+            this.circularProgressBar1.Text = "20";
+            this.circularProgressBar1.TextMode = CircularProgressBar._TextMode.Value;
+            this.circularProgressBar1.Value = ((long)(20));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 312);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.circularProgressBar3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.circularProgressBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.circularProgressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Projet 2020";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private CircularProgressBar circularProgressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private CircularProgressBar circularProgressBar2;
+        private System.Windows.Forms.Label label3;
+        private CircularProgressBar circularProgressBar3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Projet 2020/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Projet_2020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Initialisation de la fenêtre
+            InitializeComponent();
+            //Acquisition des ports COM et affichage pour permettre à l'utilistateur de selectionner le port souhaité
+            string[] serialPort = SerialPort.GetPortNames();
+
+            foreach(string s in serialPort)
+            {
+                comboBox1.Items.Add(s);
+            }
+            //Permet de récuperer les paramètres sauvegardés par l'utilisateur (ou affichage des parametres par défaut si aucun trouvé)
+            if(Properties.Settings.Default.portName != "")
+            {
+                comboBox1.Text = Properties.Settings.Default.portName;
+            }
+
+            if (Properties.Settings.Default.baudsRate != 0)
+            {
+                numericUpDown1.Value = Properties.Settings.Default.baudsRate;
+            }
+
+            if (Properties.Settings.Default.tempUnit != "")
+            {
+                comboBox2.Text = Properties.Settings.Default.tempUnit;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Bouton ouvrir port
+        {
+            //Ajoute une fonction personnalisée quand l'évenement DataReceived sera déclanché (en cas de données reçues)
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
+            //Configuration du port avec les parametres selectionnés par l'utilisateur
+            serialPort1.PortName = comboBox1.Text;
+            serialPort1.BaudRate = Decimal.ToInt32(numericUpDown1.Value);
+            //On ouvre le port
+            serialPort1.Open();
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e) //Si des données sont reçues
+        {
+            //On récupere les données dans une variable de type string
+            string data = serialPort1.ReadLine();
+            //On appel la fonction UpdateUI et on lui donne la variable contenant les données
+            UpdateUI(data);
+        }
+
+        public void UpdateUI(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(UpdateUI), new object[] { value });
+                return;
+            }
+            //On supprime les caractères vides
+            string result = value.Replace(" ", "");
+            //On récupere les differentes données
+            string[] data = result.Split('|');
+            string adcValue = Regex.Replace(data[1], @"[^\d]", "");
+            //On les affiches
+            switch (data[0])
+            {
+                case "0":
+
+                    circularProgressBar1.Value = int.Parse(adcValue);
+                    break;
+
+                case "1":
+                    circularProgressBar2.Value = int.Parse(adcValue);
+                    break;
+
+                case "2":
+                    circularProgressBar3.Value = int.Parse(adcValue);
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Bouton fermer port
+        {
+            //On ferme le port
+            serialPort1.Close();
+            circularProgressBar1.Value = 0;
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Bouton enregistrer
+        {
+            //Sauvegarde les parametres définis par l'utilisateur
+            Properties.Settings.Default.portName = comboBox1.Text;
+            Properties.Settings.Default.baudsRate = Decimal.ToInt32(numericUpDown1.Value);
+            Properties.Settings.Default.tempUnit = comboBox2.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
